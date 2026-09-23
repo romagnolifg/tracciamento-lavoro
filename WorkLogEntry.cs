7dfad1103e6ebfd5402681d0d90adebf2f9c54e8
romagnolifg <riccardo.romagnoli@focacciagroup.com>
2026-09-23 14:30:28 +0200

    feat: add WorkLogEntry helper class

---
+++ WorkLogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tracciamento_lavoro
+{
+    public class WorkLogEntry
+    {
+        public string Code { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Elapsed { get; set; }
+    }
+}
